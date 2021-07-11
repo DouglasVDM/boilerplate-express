d7f7d112018db5f932c57c1023f81ebc372a6fc0
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-11 19:23:21 +0200

    initial commit to start server

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, (req, res) => {
+  console.log(`Hello World! Server started on port: ${PORT}`)
+})
 
 
 
