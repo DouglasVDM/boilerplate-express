4b501b83878aacd31a971aad7b111ce45aebec78
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-13 17:55:52 +0200

    3.Serve and HTML File

--- myApp.js
+++ myApp.js
@@ -2,14 +2,20 @@ var express = require('express');
 var app = express();
 
 // 1.Meet the Node Console
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send(console.log('Hello World'));
-})
+});
 
 // 2.Start a Working Express Server
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send(console.log('Hello World'));
-})
+});
+
+// 3.Serve an HTML File
+const absolutePath = __dirname + '/views/index.html';
+app.get('/', (req, res) => {
+  res.sendFile(absolutePath);
+});
 
 module.exports = app;
 
